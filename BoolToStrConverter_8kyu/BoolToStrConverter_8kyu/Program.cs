07273edf73f3ec5d8945028beb62ad3b3418cd76
Aleksandr Ataakgayev <bad.bad.jaguar@gmail.com>
2018-10-18 23:46:29 +0300

    BoolToStrConverter_8kyu

---
+++ BoolToStrConverter_8kyu/BoolToStrConverter_8kyu/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace BoolToStrConverter_8kyu
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine(Kata.BoolToWord(true));
+            Console.WriteLine(Kata.BoolToWord(false));
+            Console.ReadKey();
+        }
+
+        public static class Kata
+        {
+            public static string BoolToWord(bool word) =>
+                word.Equals(true) ? "Yes" : "No";
+        }
+    }
+}
