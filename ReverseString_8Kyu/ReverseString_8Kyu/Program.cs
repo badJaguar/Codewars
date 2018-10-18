2b3f73296ea80b9eff8f29f7275a098988385845
Aleksandr Ataakgayev <bad.bad.jaguar@gmail.com>
2018-10-18 23:16:35 +0300

    ReverseString_8Kyu

---
+++ ReverseString_8Kyu/ReverseString_8Kyu/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+namespace ReverseString_8Kyu
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine(Kata.Solution("world"));
+            Console.ReadLine();
+        }
+    }
+
+    public static class Kata
+    {
+        public static string Solution(string str) =>
+            new string(str.ToCharArray().Reverse().ToArray());
+    }
+}
