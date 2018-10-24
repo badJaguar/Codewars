2b45dd3b036c8697de43bd381dbd0f12b75ceb3c
Aleksandr Ataakgayev <bad.bad.jaguar@gmail.com>
2018-10-24 23:08:53 +0300

    Printer_Errors_7kyu

---
+++ Printer Errors_7kyu/Printer Errors_7kyu/Program.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Printer_Errors_7kyu
+{
+    class Program
+    {
+        static void Main()
+        {
+            var watch = new Stopwatch();
+            watch.Start();
+            Console.WriteLine($"{Printer.PrinterError("aaaaaaaaaaaaaaaaabbbbbbbbbbbbbbbbbbmmmmmmmmmmmmmmmmmmmxyz")}" +
+                              $" {watch.ElapsedMilliseconds}");
+            Console.ReadKey();
+        }
+    }
+
+    public class Printer
+    {
+        public static string PrinterError(string s)
+        {
+            var regexError = new Regex("[n-z]", RegexOptions.Compiled);
+            var regErr = regexError.Matches(s);
+            return $"{regErr.Count}/{s.Length}";
+        }
+    }
+}
