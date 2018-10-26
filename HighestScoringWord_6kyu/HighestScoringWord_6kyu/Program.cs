3d1f2baf735f77149e515edb68db95a2a2d6b94c
Alexander Ataakgayev <bad.bad.jaguar@gmail.com>
2018-10-26 12:24:19 +0300

    HighestScoringWord_6kyu
    begone

---
+++ HighestScoringWord_6kyu/HighestScoringWord_6kyu/Program.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace HighestScoringWord_6kyu
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine(Kata.High("what time are we climbing up to the volcano"));
+
+            Console.ReadKey();
+        }
+    }
+
+    public class Kata
+    {
+        public static string High(string s)
+        {
+            var regex = new Regex("[A-Z]");
+
+            var e = s.ToLower().Remove(' ').ToCharArray();
+            foreach (var VARIABLE in e)
+            {
+                var index = (int)VARIABLE % 32;
+                Console.WriteLine(index);
+            }
+            return string.Empty;
+        }
+    }
+}
